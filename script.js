986408c9768ed970e1d0ff4063da782559ed89b2
MohamedG92 <iraqimohamed92@email.com>
2026-05-12 12:45:31 +0100

    phone

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello, World!");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("message");
+
+btnMessage.addEventListener("click", function() {
+    message.textContent = "Button clicked!";
+});
